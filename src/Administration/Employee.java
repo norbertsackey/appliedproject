d8dbd0b1e5f45e8d72347d51667245a130752607
norbysnow <norbertsackey@gmail.com>
2016-02-05 19:38:12 +0000

    Added Employee Class to Admin module

---
+++ src/Administration/Employee.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administration;
+
+/**
+ *
+ * @author Jacque
+ */
+public class Employee {
+
+    protected int empID;
+    protected String firstName;
+    protected String lastName;
+    protected String birthDate;
+    protected String gender;
+    protected int imageID;
+    protected int salary;
+    protected String position;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    protected String empType;
+    protected String nationality;
+    protected String dateEmployed;
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(int imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmpType() {
+        return empType;
+    }
+
+    public void setEmpType(String empType) {
+        this.empType = empType;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateEmployed() {
+        return dateEmployed;
+    }
+
+    public void setDateEmployed(String dateEmployed) {
+        this.dateEmployed = dateEmployed;
+    }
+
+}
